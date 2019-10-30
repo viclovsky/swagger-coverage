7a03084995fa5b50bf4ab8ca9883a2eca7c47596
Victor Orlovsky <viclovsky@gmail.com>
2019-10-30 17:47:01 +0300

    add RequestLoggerFilterTest

--- rest-assured-request-logger-filter/build.gradle.kts
+++ rest-assured-request-logger-filter/build.gradle.kts
@@ -13,6 +13,8 @@ dependencies {
     compile("io.rest-assured", "rest-assured", "4.0.0")
     compile("io.swagger", "swagger-compat-spec-parser", "1.0.36")
     testCompile("junit", "junit", "4.12")
+    testCompile("com.github.tomakehurst", "wiremock", "2.24.1")
+    testCompile("org.hamcrest", "hamcrest-all", "1.3")
 }
 
 configure<JavaPluginConvention> {
