aec2f65e98bc42f62c1aa3445862524a121f76e8
a.makovetskiy <a.makovetskiy@a1-systems.com>
2020-03-10 20:56:26 +0300

    use petshop data for test

--- swagger-coverage-by-branch/src/test/java/com/github/viclovsky/swagger/coverage/branch/test/SimpleTest.java
+++ swagger-coverage-by-branch/src/test/java/com/github/viclovsky/swagger/coverage/branch/test/SimpleTest.java
@@ -3,16 +3,23 @@ package com.github.viclovsky.swagger.coverage.branch.test;
 import com.github.viclovsky.swagger.coverage.branch.generator.Generator;
 import org.junit.Test;
 
+import java.io.File;
 import java.nio.file.*;
 
 public class SimpleTest {
+
+    private static final String OUTPUT_SWAGGER_COVERAGE_DIR = "swagger-coverage-output";
+    private static final String SPEC_FILE = "petstory.json";
+
+    private File spec = new File(getClass().getClassLoader().getResource(SPEC_FILE).getFile());
+    private File reqDir = new File(getClass().getClassLoader().getResource(OUTPUT_SWAGGER_COVERAGE_DIR).getFile());
 
     @Test
     public void test1(){
         Generator generator = new Generator();
         generator
-            .setInputPath(Paths.get("/home/amakovetskiy/work/mts-by-family-backend-test/target/swagger-coverage-output/"))
-            .setSpecPath(Paths.get("/home/amakovetskiy/work/mts-by-family-backend-test/swagger.json"))
+            .setInputPath(Paths.get(String.valueOf(reqDir.toPath())))
+            .setSpecPath(Paths.get(String.valueOf(spec.toPath())))
             .run();
     }
 
