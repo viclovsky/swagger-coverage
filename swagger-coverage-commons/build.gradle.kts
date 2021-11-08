37e1e86c4ce3ea57a6dae3d7f0a0687b63c7e5ff
Victor Orlovsky <viclovsky@gmail.com>
2021-11-08 15:33:40 +0300

    Add dirty fix for https://github.com/viclovsky/swagger-coverage/issues/90

--- swagger-coverage-commons/build.gradle.kts
+++ swagger-coverage-commons/build.gradle.kts
@@ -12,6 +12,8 @@ repositories {
 dependencies {
     implementation("org.freemarker:freemarker")
     implementation("io.swagger:swagger-models")
+    //dirty fix https://github.com/viclovsky/swagger-coverage/issues/90
+    implementation("io.swagger.core.v3:swagger-core")
     implementation("io.swagger.core.v3:swagger-models")
     implementation("org.slf4j:slf4j-simple")
     implementation("log4j:log4j")
